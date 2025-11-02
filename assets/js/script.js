0964e48a960e9dc8c39bab8248d7d3deff061fd2
NIDA MUZAFFAR <nidamuzaffar606@gmail.com>
2025-11-02 19:08:05 +0500

    make responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,24 @@
-const cartButtons = document.querySelectorAll('.cart-button');
+const bar = document.getElementById('bar');
+const close = document.getElementById('close');
+const nav = document.getElementById('navbar');
 
-cartButtons.forEach(button => {
+if (bar) {
+    bar.addEventListener('click', () => {
+        nav.classList.add('active')
+        
+    })
+}
+
+if (close) {
+    close.addEventListener('click', () => {
+        nav.classList.remove('active')
+    })
+}  
+
+
+// shop button
+const cartButton = document.querySelectorAll('.cart-button');
+cartButton.forEach(button => {
     button.addEventListener('click', cartClick);
 });
 
